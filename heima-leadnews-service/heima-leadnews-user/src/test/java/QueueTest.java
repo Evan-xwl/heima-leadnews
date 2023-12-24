a55025c5475089bb6ba88294f550c9f327c005c0
xwl <1139323910@qq.com>
2023-12-24 12:04:32 +0800

    day1:初始化工程，创建网关及登录接口

---
+++ heima-leadnews-service/heima-leadnews-user/src/test/java/QueueTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author ruoling
+ * @date 2023/12/23 22:59:12
+ * @description
+ */
+public class QueueTest {
+    @Test
+    public void testQueue() {
+        PriorityQueue<Integer>
+                queue = new PriorityQueue<>((i, j) -> i - j);
+        queue.add(1);
+        queue.add(3);
+        queue.add(2);
+        queue.add(4);
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        queue.poll();
+    }
+}
